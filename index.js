b7f4293a44c70f831c92be03079667418f07c746
Broken Tiger <59955785+Github2786@users.noreply.github.com>
2023-06-27 16:30:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// import "expo-router/entry";
+
+import { registerRootComponent } from 'expo';
+import App from './App';
+import { Router } from 'expo-router';
+
+// Create a new instance of Router
+const router = new Router(App);
+
+// Registers the root component of your app
+registerRootComponent(router.getComponent());
+
